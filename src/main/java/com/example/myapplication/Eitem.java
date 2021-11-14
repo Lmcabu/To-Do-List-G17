6e2450fd27c3245995681c365eb86f525d2927d5
BenChan256 <71311105+BenChan256@users.noreply.github.com>
2021-11-14 18:33:53 +0800

    Add files via upload

---
+++ src/main/java/com/example/myapplication/Eitem.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+public class Eitem {
+    private int mImageResource;
+    private String mText1;
+    private String mText2;
+
+    public Eitem(int imageResource, String text1, String text2){
+        mImageResource = imageResource;
+        mText1 = text1;
+        mText2 = text2;
+    }
+
+    public void changeText1(String text){
+        mText1 = text;
+    }
+
+    public int getmImageResource(){
+        return mImageResource;
+    }
+    public String getText1(){
+        return mText1;
+    }
+    public String getText2(){
+        return mText2;
+    }
+}
