2abaaac8cf91161c505bfaab325d36699cb19010
Lmcabu <1547768541@qq.com>
2021-11-17 13:14:18 +0800

    add Xavier's code

---
+++ app/src/main/java/com/example/version1/ExampleDialog.java
@@ -0,0 +1,65 @@
+package com.example.version1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+    private EditText editTextTitle;
+    private EditText editTextDetail;
+    private ExampleDialogListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_example_dialog, null);
+
+        builder.setView(view)
+                .setTitle("Task")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String username = editTextTitle.getText().toString();
+                        String password = editTextDetail.getText().toString();
+                        listener.applyTexts(username, password);
+                    }
+                });
+        editTextTitle = view.findViewById(R.id.edit_Title);
+        editTextDetail = view.findViewById(R.id.edit_detail);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (ExampleDialogListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + "must implement ExampleDialogListener");
+        }
+    }
+
+    public interface ExampleDialogListener{
+        void applyTexts(String username, String password);
+    }
+}
---
+++ app/src/main/java/com/example/version1/List2.java
@@ -0,0 +1,41 @@
+package com.example.version1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class List2 extends AppCompatActivity implements ExampleDialog.ExampleDialogListener {
+    private TextView textViewTitle;
+    private TextView textViewDetail;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewTitle = (TextView) findViewById(R.id.textview_Title);
+        textViewDetail = (TextView) findViewById(R.id.textview_Detail);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog(){
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    @Override
+    public void applyTexts(String username, String password) {
+        textViewTitle.setText(username);
+        textViewDetail.setText(password);
+    }
+}
