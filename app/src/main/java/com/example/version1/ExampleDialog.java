36c28060910450f2750b46d2b6da34b66edf0fea
Lmcabu <1547768541@qq.com>
2021-11-17 14:49:32 +0800

    debug

--- app/src/main/java/com/example/version1/ExampleDialog.java
+++ app/src/main/java/com/example/version1/ExampleDialog.java
@@ -1,5 +1,4 @@
 package com.example.version1;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -24,7 +23,7 @@ public class ExampleDialog extends AppCompatDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.activity_example_dialog, null);
+        View view = inflater.inflate(R.layout.fragment_example_dialog, null);
 
         builder.setView(view)
                 .setTitle("Task")
