846016516c2ca3c6e0d9dbd674efc112205ba821
[TingYu CHAN] <[ting9518@gmail.com]>
2021-11-20 16:15:02 +0800

    Test

--- app/src/main/java/com/example/version1/ListActivity.java
+++ app/src/main/java/com/example/version1/ListActivity.java
@@ -35,7 +35,7 @@ public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
 
         createExampleList();
         buildRecyclerView();
